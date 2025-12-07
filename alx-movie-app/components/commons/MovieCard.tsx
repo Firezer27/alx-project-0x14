2297c1cb3e0c8c7a3d6e6b6ff884381fffe0151c
Firezer27 <firezer.bizuwork27@gmail.com>
2025-12-07 14:35:12 +0300

    set up movies app

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { MovieProps } from "@/interfaces";
+import Image from "next/image";
 
-const MovieCard: React.FC = () => {
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
   return (
-    <div className="border p-4 rounded shadow-sm">
-      <h3 className="font-semibold">Movie Title</h3>
-      <p className="text-sm text-gray-600">Movie description here...</p>
+    <div className="h-[563px]">
+      <div>
+        <Image
+          className="h-[430px] w-full rounded-md hover:cursor-pointer"
+          src={posterImage}
+          width={100}
+          height={100}
+          alt={title}
+        />
+      </div>
+
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
     </div>
   );
 };
